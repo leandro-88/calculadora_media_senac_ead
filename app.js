4bdd0d51e87cec306b532b32e7224f626f84de1d
Leandro Filho <71965502+leandro-88@users.noreply.github.com>
2024-09-28 17:49:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,23 @@ function sortear() {
     let quantidade = parseFloat(converterDecimal(document.getElementById("quantidade").value));
     let de = parseFloat(converterDecimal(document.getElementById("de").value));
     let ate = parseFloat(converterDecimal(document.getElementById("ate").value));
+    let ambientacao = parseFloat(converterDecimal(document.getElementById("ambientacao").value));
 
+    // Validação dos campos
     if (isNaN(quantidade) || isNaN(de) || isNaN(ate)) {
         alert("Por favor, insira apenas números válidos.");
         reiniciar();
         return;
     }
 
+    // Se ambientacao for NaN, define como 0
+    if (isNaN(ambientacao)) {
+        ambientacao = 0;
+    }
+
     let resultado = document.getElementById("resultado");
-    let media = calcularesultado(quantidade, de, ate);
-    
+    let media = calcularesultado(quantidade, de, ate, ambientacao);
+
     // Arredondar para duas casas decimais e formatar com vírgula
     resultado.innerHTML = `<label class="texto__paragrafo">Sua média é: <strong>${media.toFixed(2).replace('.', ',')}</strong></label>`;
     habilitarBotaoReiniciar();
@@ -22,8 +29,8 @@ function converterDecimal(valor) {
     return valor.replace(',', '.');
 }
 
-function calcularesultado(quantidade, de, ate) {
-    let media = (quantidade * 0.2) + (de * 0.2) + (ate * 0.6);
+function calcularesultado(quantidade, de, ate, ambientacao) {
+    let media = (quantidade * 0.2) + (de * 0.2) + (ate * 0.6) + ambientacao;
     return media;
 }
 
@@ -43,7 +50,7 @@ function reiniciar() {
     document.getElementById("quantidade").value = "";
     document.getElementById("de").value = "";
     document.getElementById("ate").value = "";
-
+    document.getElementById("ambientacao").value = ""; // Adicionei esta linha para reiniciar o campo ambientacao
     document.getElementById("resultado").innerHTML = `<label class="texto__paragrafo">Números sorteados: nenhum até agora</label>`;
     desabilitarBotaoReiniciar();
 }
